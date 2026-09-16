90519bc7f5b16f8d898539a028a8f989d37165f4
David Hajdarovic <hajdarovicdavid@gmail.com>
2026-09-16 12:56:47 +0200

    px updated

--- resources/views/components/ui/module/tab-link-divider.blade.php
+++ resources/views/components/ui/module/tab-link-divider.blade.php
@@ -1,4 +1,4 @@
 {{-- Vertical line between two module tab links (enabled per route with the "Divider after" switch in app settings) --}}
-<li class="nav-item d-flex py-2 px-1" aria-hidden="true">
+<li class="nav-item d-flex py-2 px-2" aria-hidden="true">
     <div class="vr"></div>
 </li>
